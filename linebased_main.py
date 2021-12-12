346ec215dfb7ddb3eb5105e68cde86de0f8c5c95
Johannes U <ju93@gmx.net>
2021-12-12 19:07:10 +0100

    simplified

--- linebased_main.py
+++ linebased_main.py
@@ -3,9 +3,15 @@ Created on 09.12.2021
 
 @author: pucgenie+aoc212@gmail.com
 '''
+def apply_filters(data_filter, data_parser, content_lines,):
+	if data_filter:
+		content_lines = filter(data_filter, content_lines,)
+	if data_parser:
+		content_lines = map(data_parser, content_lines,)
+	return content_lines
 
 def linebased_main(puzzle_name, do_it, example_data=None,
-		data_filter = None, data_parser=None, data_url=None, parse_example_data=False,
+		data_filter=None, data_parser=None, data_url=None, parse_example_data=False,
 		argparse_extras=None,
 	):
 	import argparse
@@ -19,10 +25,7 @@ def linebased_main(puzzle_name, do_it, example_data=None,
 		print("mode: test")
 		content_lines = example_data
 		if parse_example_data:
-			if data_filter:
-				content_lines = filter(data_filter, content_lines,)
-			if data_parser:
-				content_lines = map(data_parser, content_lines,)
+			content_lines = apply_filters(data_filter, data_parser, content_lines,)
 		do_it(
 			content_lines,
 			args=args,
@@ -32,11 +35,7 @@ def linebased_main(puzzle_name, do_it, example_data=None,
 	# https://docs.python.org/3/library/urllib.request.html recommends requests module instead, so...
 	import requests
 	with requests.get(data_url, timeout=5, cookies={'session': args.session,}) as content:
-		content_lines = content.text.splitlines()
-		if data_filter:
-			content_lines = filter(data_filter, content_lines,)
-		if data_parser:
-			content_lines = map(data_parser, content_lines,)
+		content_lines = apply_filters(data_filter, data_parser, content.text.splitlines(),)
 		do_it(
 			content_lines,
 			args=args,
