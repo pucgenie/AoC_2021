48dae70eae78f3b479376e921db1b2d7a747ca9c
Johannes U <ju93@gmx.net>
2021-12-12 20:26:59 +0100

    slightly better performance
    Uses a fraction of RAM but still fails to deliver for 256 days simulation time in less than 10 minutes calculation time

--- Lanternfish.py
+++ Lanternfish.py
@@ -3,34 +3,42 @@ Created on 12.12.2021
 
 @author: pucgenie+aoc212@gmail.com
 '''
+from _collections import deque
 def int_positive(n):
 	if n == '0':
 		raise ValueError("Unexpected input")
 	return int(n)
 
-def parse_sane_input(line):
-	return (int_positive(n) for n in line.split(','))
-
 if __name__ == '__main__':
 	def do_it(data_in, args,):
-		fish = list(*data_in)
-		for _ in range(80):
-			num_zeroes = 0
+		fish = list(data_in)
+		del data_in
+		
+		days = args.days
+		del args
+		
+		born_at_day = [0] * days
+		successors = deque()
+		for day in range(days):
 			for i in range(len(fish)):
 				# can't use %7 because of 8.
-				predecessor = (fish[i] - 1)
+				predecessor = fish[i] - 1
 				if predecessor == -1:
 					predecessor = 6
-					num_zeroes += 1
+					
+					born_at_day[day] += 1
+					successors.append(day)
+					while successors:
+						for future_day in range(successors.pop() + 9, days, 7,):
+							born_at_day[future_day] += 1
+							successors.append(future_day)
+					
+					#fish.append(8)
 				fish[i] = predecessor
-			while num_zeroes > 0:
-				fish.append(8)
-				num_zeroes -= 1
-			#print(fish)
 		
 		from stdout_tools import print_outcome
 		print_outcome(
-			count=len(fish),
+			count=len(fish) + sum(born_at_day),
 			#bitmap=bitmap,
 		)
 	
@@ -39,9 +47,10 @@ if __name__ == '__main__':
 		"#6 Lanternfish",
 		do_it,
 		example_data=(
-			'3,4,3,1,2',
+			3,4,3,1,2,
 		),
 		data_url='https://adventofcode.com/2021/day/6/input',
-		data_parser=parse_sane_input,
-		parse_example_data=True,
+		data_parser=int_positive,
+		split_char=',',
+		argparse_extras=lambda parser : parser.add_argument('--days', type=int, default=80, help="""Count of days to simulate.""",)
 	)
