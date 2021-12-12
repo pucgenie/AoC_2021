94bcb7a3c6fa5b7ad79ca22bca3c8436ceaf9977
Johannes U <ju93@gmx.net>
2021-12-12 20:49:10 +0100

    still not fast enough

--- Lanternfish.py
+++ Lanternfish.py
@@ -10,41 +10,33 @@ def int_positive(n):
 	return int(n)
 
 if __name__ == '__main__':
-	def do_it(data_in, args,):
-		fish = list(data_in)
-		del data_in
-		
+	def do_it(fish, args,):
 		days = args.days
 		del args
 		
 		born_at_day = [0] * days
 		successors = deque()
-		for day in range(days):
-			for i in range(len(fish)):
-				# can't use %7 because of 8.
-				predecessor = fish[i] - 1
-				if predecessor == -1:
-					predecessor = 6
-					
-					born_at_day[day] += 1
-					successors.append(day)
-					while successors:
-						for future_day in range(successors.pop() + 9, days, 7,):
-							born_at_day[future_day] += 1
-							successors.append(future_day)
-					
-					#fish.append(8)
-				fish[i] = predecessor
+		for z in fish:
+			# On the first day there were born some fish that were almost able to give birth again
+			born_at_day[0] += 1
+			for future_day in range(z, days, 7,):
+				born_at_day[future_day] += 1
+				successors.append(future_day)
+		while successors:
+			print(len(successors))
+			for future_day in range(successors.pop() + 9, days, 7,):
+				born_at_day[future_day] += 1
+				successors.append(future_day)
 		
 		from stdout_tools import print_outcome
 		print_outcome(
-			count=len(fish) + sum(born_at_day),
+			count=sum(born_at_day),
 			#bitmap=bitmap,
 		)
 	
 	from linebased_main import linebased_main
 	linebased_main(
-		"#6 Lanternfish",
+		"#6 Lanternfish part 2",
 		do_it,
 		example_data=(
 			3,4,3,1,2,
@@ -52,5 +44,5 @@ if __name__ == '__main__':
 		data_url='https://adventofcode.com/2021/day/6/input',
 		data_parser=int_positive,
 		split_char=',',
-		argparse_extras=lambda parser : parser.add_argument('--days', type=int, default=80, help="""Count of days to simulate.""",)
+		argparse_extras=lambda parser : parser.add_argument('--days', type=int, default=256, help="""Count of days to simulate.""",)
 	)
